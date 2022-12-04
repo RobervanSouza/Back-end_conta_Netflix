060c9270f08eef0cd5c9b701e69e63408b9f9872
Robervan Souza <rob.robervan@hotmail.com>
2022-12-04 08:37:14 -0300

    create conta dto

--- series-favoritas/src/conta/dto/create-conta.dto.ts
+++ series-favoritas/src/conta/dto/create-conta.dto.ts
@@ -1,1 +1,12 @@
-export class CreateContaDto {}
+import { Iusuarios } from 'src/Users/UsuariosInterface/usuarios';
+
+export class CreateContaDto {
+  series: string;
+  filmes: string;
+  documentarios: string;
+  desenhos: string;
+  shows: string;
+  usuarios: Iusuarios[];
+  admin: Iusuarios[];
+  listaUsuarios: string;
+}
