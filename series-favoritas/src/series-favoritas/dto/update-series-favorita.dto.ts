ced881d8c6aff5f75e57c519842cec005775b0d9
Robervan Souza <rob.robervan@hotmail.com>
2022-12-04 07:59:45 -0300

    estruturando as entidades

--- series-favoritas/src/series-favoritas/dto/update-series-favorita.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSeriesFavoritaDto } from './create-series-favorita.dto';
-
-export class UpdateSeriesFavoritaDto extends PartialType(CreateSeriesFavoritaDto) {}
--- series-favoritas/src/series-favoritas/series-favoritas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SeriesFavoritasService } from './series-favoritas.service';
-import { SeriesFavoritasController } from './series-favoritas.controller';
-
-@Module({
-  controllers: [SeriesFavoritasController],
-  providers: [SeriesFavoritasService]
-})
-export class SeriesFavoritasModule {}
