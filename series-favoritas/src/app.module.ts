81b11dab6770faec44b9fe0e4ab309f5101d1e8f
Robervan Souza <rob.robervan@hotmail.com>
2022-12-12 23:51:35 -0300

    okkk

--- series-favoritas/src/app.module.ts
+++ series-favoritas/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DataBaseModule } from 'src/prisma/service&database/database.module';
 import { AuthModule } from './auth/auth.module';
-
 import { ContaModule } from './conta/conta.module';
 import { ListaUsuariosModule } from './lista-usuarios/lista-usuarios.module';
-
 import { UserModule } from './users/user.module';
 
 @Module({
